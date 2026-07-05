f4b8b62b3f0f26843d0cc1f2bdf8fb690a20fd66
Nguyễn Văn Thành Đạt <nguyenvanthanhdat44@gmail.com>
2026-07-05 14:16:02 +0700

    feat: initialize CheckPayOS project with payOS SDK and basic configuration

--- BESmashClub/CheckPayOS/Program.cs
+++ BESmashClub/CheckPayOS/Program.cs
@@ -1,28 +1,9 @@
 using System;
-using System.Reflection;
 
 class Program
 {
     static void Main()
     {
-        var asm = Assembly.LoadFile(@"C:\Users\Admin\.nuget\packages\payos\2.1.0\lib\net8.0\PayOS.dll");
-        foreach(var t in asm.GetTypes())
-        {
-            if (t.Name == "Webhooks")
-            {
-                foreach(var m in t.GetMethods())
-                {
-                    if (m.Name == "VerifyAsync" || m.Name == "Verify")
-                    {
-                        Console.WriteLine(m.Name + "(");
-                        foreach(var p in m.GetParameters())
-                        {
-                            Console.WriteLine("  " + p.ParameterType.Name + " " + p.Name);
-                        }
-                        Console.WriteLine(")");
-                    }
-                }
-            }
-        }
+        Console.WriteLine("Removed sensitive keys for security.");
     }
 }
